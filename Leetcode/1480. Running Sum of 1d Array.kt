d77aeeabbcedfc36a642b569ee23811823baaedc
MuxauJI <muxauji_sh@mail.ru>
2022-06-25 00:59:01 +0300

    LeetCode 75
    Day 1
    Prefix Sum
    
    1480. Running Sum of 1d Array
    724. Find Pivot Index

---
+++ Leetcode/1480. Running Sum of 1d Array.kt
@@ -0,0 +1,15 @@
+package Leetcode
+
+fun runningSum(nums: IntArray): IntArray {
+    val res = mutableListOf<Int>()
+    var sum = 0
+    nums.forEach {
+        sum += it
+        res.add(sum)
+    }
+    return res.toIntArray()
+}
+
+fun main() {
+    runningSum(intArrayOf(1,2,3,4)).also(::println)
+}
---
+++ Leetcode/724. Find Pivot Index.kt
@@ -0,0 +1,17 @@
+package Leetcode
+
+fun pivotIndex(nums: IntArray): Int {
+    var sum_num = 0
+    val sum_all = nums.sum()
+    nums.forEachIndexed  { i, el ->
+        if(sum_all - el - sum_num == sum_num) {
+            return i
+        }
+        sum_num += el
+    }
+    return -1
+}
+
+fun main() {
+    pivotIndex(intArrayOf(1,7,3,6,5,6)).also(::println)
+}
